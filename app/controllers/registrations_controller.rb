ac7253732552bdd2e6ee838e59135e6baabcd246
harmolipi <nikob381@gmail.com>
2021-08-25 20:55:16 -0400

    Added basic user registration and authentication

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,13 @@
-class RegistrationsController < Devise:RegistrationsController
+class RegistrationsController < Devise::RegistrationsController
 
   private
 
   def sign_up_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_configuration)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name, :username, :email, :password, :password_configuration, :current_password)
+    params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :current_password)
   end
+
+end
